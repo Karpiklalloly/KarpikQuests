06b48328c682bdff81d1bee839173b6e9c8d2473
Karpik <artem19051664@gmail.com>
2023-06-16 22:24:06 +0300

    Add Quest's and QuestTask's bases for protected methods and properties

---
+++ Interfaces/QuestTaskBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace KarpikQuests.Interfaces
+{
+    public abstract class QuestTaskBase : IQuestTask
+    {
+        public abstract string Name { get; }
+
+        public abstract IQuestTask.TaskStatus Status { get; }
+        bool IQuestTask.CanBeCompleted
+        {
+            get => CanBeCompleted;
+            set => CanBeCompleted = value;
+        }
+        public bool CanBeCompleted { get; protected set; }
+
+        public abstract event Action<IQuestTask> Completed;
+
+        public abstract void Init(string name);
+
+        void IQuestTask.Complete()
+        {
+            Complete();
+        }
+        protected abstract void Complete();
+
+        void IQuestTask.ForceBeCompleted()
+        {
+            ForceBeCompleted();
+        }
+        protected abstract void ForceBeCompleted();
+    }
+}
