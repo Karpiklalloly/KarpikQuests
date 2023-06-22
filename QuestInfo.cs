d5ce1b0bb56e381a432534c76146ba95d528c580
Karpik <artem19051664@gmail.com>
2023-06-22 17:18:23 +0300

    Add QuestInfo static class for getting info about quests

---
+++ QuestInfo.cs
@@ -0,0 +1,36 @@
+﻿using KarpikQuests.Interfaces;
+using System.Collections.Generic;
+
+namespace KarpikQuests
+{
+    public static class QuestInfo
+    {
+        private static readonly List<IQuestAggregator> _aggregators = new List<IQuestAggregator>();
+
+        public static void RegisterAggregator(IQuestAggregator aggregator)
+        {
+            if (_aggregators.Contains(aggregator))
+            {
+                return;
+            }
+
+            _aggregators.Add(aggregator);
+        }
+
+        public static IQuest GetQuest(string key)
+        {
+            foreach (var item in _aggregators)
+            {
+                foreach (var quest in item.Quests)
+                {
+                    if (quest.Key.Equals(key))
+                    {
+                        return quest;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
