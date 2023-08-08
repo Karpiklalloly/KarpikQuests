8eb43dea0b4fe8f0a7a6e93ec2a9fbe0f1534f91
Karpik <artem19051664@gmail.com>
2023-08-08 21:01:35 +0300

    Fix bug in irderly task processor

--- QuestTaskProcessorTypes/QuestTaskProcessorOrderly.cs
+++ QuestTaskProcessorTypes/QuestTaskProcessorOrderly.cs
@@ -10,12 +10,12 @@ namespace KarpikQuests.QuestTaskProcessorTypes
     {
         public void OnTaskCompleted(IEnumerable<IQuestTask> tasks, IQuestTask task)
         {
-            var index = tasks.TakeWhile(x => x.Equals(task)).Count();
-            if (tasks.Last().Equals(task))
+            var list = tasks.ToList();
+            var index = list.IndexOf(task);
+            if (tasks.Count() == index + 1)
             {
                 return;
             }
-
             tasks.ElementAt(index + 1).Reset(true);
         }
 
