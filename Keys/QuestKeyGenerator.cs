7b1116fb8fd776600cb536d8ed9fcdf656998adf
Karpik <artem19051664@gmail.com>
2023-11-12 12:12:01 +0300

    Upgrade key generator

--- Keys/QuestKeyGenerator.cs
+++ Keys/QuestKeyGenerator.cs
@@ -1,17 +1,39 @@
-﻿namespace KarpikQuests.Keys
+﻿using System;
+
+namespace KarpikQuests.Keys
 {
     public static class QuestKeyGenerator
     {
-        private static uint _autoUintKey = 0;
+        private const string _defaultPrefix = "";
 
-        public static string GenerateNextAutoKey()
+        public static string GenerateNextAutoKey(string prefix = _defaultPrefix)
         {
-            return _autoUintKey++.ToString();
+            string key = Guid.NewGuid().ToString();
+            if (prefix == _defaultPrefix)
+            {
+                return key;
+            }
+            return prefix + key;
         }
 
-        public static string[] GenerateNextAutoKeys(int count)
+        public static Guid GenerateNextAutoKey()
+        {
+            return Guid.NewGuid();
+        }
+
+        public static string[] GenerateNextAutoKeys(int count, string prefix = _defaultPrefix)
         {
             string[] keys = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                keys[i] = GenerateNextAutoKey(prefix);
+            }
+            return keys;
+        }
+
+        public static Guid[] GenerateNextAutoKeys(int count)
+        {
+            Guid[] keys = new Guid[count];
             for (int i = 0; i < count; i++)
             {
                 keys[i] = GenerateNextAutoKey();
