ded41a2a4bb87c877ae9b60cb22e19b982a799b8
Karpik <artem19051664@gmail.com>
2024-01-20 18:42:14 +0300

    Add NeededCount completion type

---
+++ CompletionTypes/NeededCount.cs
@@ -0,0 +1,70 @@
+﻿using KarpikQuests.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarpikQuests.CompletionTypes
+{
+    public class NeededCount : ICompletionType
+    {
+        public readonly int Count;
+
+        public NeededCount(int count)
+        {
+            Count = count;
+        }
+
+        public bool CheckCompletion(IEnumerable<ITaskBundle> bundles)
+        {
+            int curCount = 0;
+            if (!bundles.Any())
+            {
+                return false;
+            }
+
+            foreach (var bundle in bundles)
+            {
+                foreach (var task in bundle)
+                {
+                    if (task.Status == IQuestTask.TaskStatus.Completed)
+                    {
+                        curCount++;
+                    }
+                }
+            }
+
+            if (curCount >= Count)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool CheckCompletion(ITaskBundle bundle)
+        {
+            int curCount = 0;
+            if (!bundle.Any())
+            {
+                return true;
+            }
+
+            foreach (var task in bundle)
+            {
+                if (task.Status == IQuestTask.TaskStatus.Completed)
+                {
+                    curCount++;
+                }
+            }
+
+            if (curCount >= Count)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
