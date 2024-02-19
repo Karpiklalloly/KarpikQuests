409a55f7cc8bffa451eca17d99782d4125cb095e
Karpik <artem19051664@gmail.com>
2024-02-19 11:07:29 +0300

    Improve QuestBuilder

--- QuestBuilder.cs
+++ QuestBuilder.cs
@@ -1,96 +1,100 @@
-﻿using KarpikQuests.Interfaces;
+﻿using KarpikQuests.CompletionTypes;
+using KarpikQuests.Factories;
+using KarpikQuests.Interfaces;
 using KarpikQuests.Keys;
 using KarpikQuests.QuestSample;
+using KarpikQuests.TaskProcessorTypes;
 using System;
 
 namespace KarpikQuests
 {
     public class QuestBuilder
     {
-        private IQuest _quest;
-        private readonly IQuestAggregator _questAggregator;
-        private bool _addToAggregator = true;
-
-        public QuestBuilder(IQuestAggregator aggregator)
+        public QuestBuilderPart Start<T>(string name, string description) where T : IQuest, new()
         {
-            if (aggregator is null) throw new ArgumentNullException(nameof(aggregator));
-
-            _questAggregator = aggregator;
+            return Start<T>(name, description, new TaskBundleCollection());
         }
 
-        public QuestBuilder()
+        public QuestBuilderPart Start<T>(string name, string description,
+            IProcessorType processor, ICompletionType completionType) where T : IQuest, new()
         {
-            _addToAggregator = false;
+            processor ??= new Disorderly();
+            completionType ??= new AND();
+            return Start<T>(name, description, new TaskBundleCollection(completionType, processor));
         }
 
-        public QuestBuilder Start<T>(string name, string description,
-            IProcessorType? processor, ICompletionType? completionType) where T : IQuest, new()
+        public QuestBuilderPart Start<T>(string name, string description,
+            ITaskBundleCollection? bundles) where T : IQuest, new()
         {
-            return Start<T>("Empty key:" + KeyGenerator.GenerateKey(), name, description, processor, completionType);
+            var quest = new T();
+
+            bundles ??= new TaskBundleCollection();
+
+            quest.Init(KeyGenerator.GenerateKey(""), name, description, bundles);
+
+            return new QuestBuilderPart(quest);
         }
 
-        public QuestBuilder Start<T>(string key, string name, string description,
-            IProcessorType? processor, ICompletionType? completionType) where T : IQuest, new()
+        public QuestBuilderPart Start(IQuest quest)
         {
-            _quest = new T();
-            _quest.Init(key, name, description, new TaskBundleCollection(completionType, processor));
-            _addToAggregator = true;
-            return this;
+            return new QuestBuilderPart((IQuest)quest.Clone());
         }
-
-        public QuestBuilder Start<T>(string key, string name, string description,
-            ITaskBundleCollection bundles) where T : IQuest, new()
+        
+        public class QuestBuilderPart
         {
-            _quest = new T();
-            _quest.Init(key, name, description, bundles);
-            _addToAggregator = true;
-            return this;
-        }
+            private IQuest _quest;
+            private IQuestAggregator _questAggregator;
+            private bool _addToAggregator;
 
-        /// <summary>
-        /// Clone quest
-        /// </summary>
-        /// <param name="quest"></param>
-        /// <returns></returns>
-        public QuestBuilder Start(IQuest quest)
-        {
-            _quest = (IQuest)quest.Clone();
-            _addToAggregator = false;
-            return this;
-        }
-
-        public QuestBuilder AddBundle(ITaskBundle bundle)
-        {
-            if (_quest.TaskBundles.Has(bundle)) throw new InvalidOperationException("Quest can't contain equel bundle");
-
-            _quest.AddBundle(bundle);
-            return this;
-        }
-
-        public QuestBuilder DoNotAddAggregatorOnCreate()
-        {
-            _addToAggregator = false;
-            return this;
-        }
-
-        public QuestBuilder OnComplete(Action<IQuest> onComplete)
-        {
-            _quest.Completed += onComplete;
-            return this;
-        }
-
-        public IQuest Create()
-        {
-            if (_quest is null) throw new InvalidOperationException("Quest is not setted");
-
-            if (_addToAggregator)
+            public QuestBuilderPart(IQuest quest)
             {
-                _questAggregator.TryAddQuest(_quest);
+                if (quest is null) throw new ArgumentNullException(nameof(quest));
+
+                _quest = quest;
+            }
+        
+            /// <summary>
+            /// Clone quest
+            /// </summary>
+            /// <param name="quest"></param>
+            /// <returns></returns>
+
+            public QuestBuilderPart AddBundle(ITaskBundle bundle)
+            {
+                if (_quest.TaskBundles.Has(bundle)) throw new InvalidOperationException("Quest can't contain equel bundle");
+
+                _quest.AddBundle(bundle);
+                return this;
             }
 
-            IQuest quest = _quest;
-            _quest = null;
-            return quest;
+            public QuestBuilderPart AddToAggregatorOnCreate(IQuestAggregator aggregator)
+            {
+                if (aggregator is null) throw new ArgumentNullException(nameof(aggregator));
+
+                _questAggregator = aggregator;
+                _addToAggregator = true;
+                return this;
+            }
+
+            public QuestBuilderPart OnComplete(Action<IQuest> onComplete)
+            {
+                _quest.Completed += onComplete;
+                return this;
+            }
+
+            public IQuest Create()
+            {
+                if (_quest is null) throw new InvalidOperationException("Quest is not setted");
+
+                if (_addToAggregator)
+                {
+                    _questAggregator.TryAddQuest(_quest);
+                }
+
+                IQuest quest = _quest;
+                _quest = null;
+                return quest;
+            }
         }
     }
 }
