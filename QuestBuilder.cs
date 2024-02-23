4e0918befff0661d524ac04b95991fbfa8bf18ba
Karpik <artem19051664@gmail.com>
2024-02-23 11:26:53 +0300

    Make static  QuestBuilder and struct QuestBuilderPart

--- QuestBuilder.cs
+++ QuestBuilder.cs
@@ -1,29 +1,31 @@
 ﻿using KarpikQuests.CompletionTypes;
-using KarpikQuests.Factories;
 using KarpikQuests.Interfaces;
 using KarpikQuests.Keys;
 using KarpikQuests.QuestSample;
 using KarpikQuests.TaskProcessorTypes;
 using System;
+using System.Runtime.CompilerServices;
 
 namespace KarpikQuests
 {
-    public class QuestBuilder
+    public static class QuestBuilder
     {
-        public QuestBuilderPart Start<T>(string name, string description) where T : IQuest, new()
+        public static QuestBuilderPart Start<T>(string name, string description) where T : IQuest, new()
         {
             return Start<T>(name, description, new TaskBundleCollection());
         }
 
-        public QuestBuilderPart Start<T>(string name, string description,
-            IProcessorType processor, ICompletionType completionType) where T : IQuest, new()
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static QuestBuilderPart Start<T>(string name, string description,
+            IProcessorType? processor, ICompletionType? completionType) where T : IQuest, new()
         {
             processor ??= new Disorderly();
             completionType ??= new AND();
             return Start<T>(name, description, new TaskBundleCollection(completionType, processor));
         }
 
-        public QuestBuilderPart Start<T>(string name, string description,
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static QuestBuilderPart Start<T>(string name, string description,
             ITaskBundleCollection? bundles) where T : IQuest, new()
         {
             var quest = new T();
@@ -35,12 +37,22 @@ namespace KarpikQuests
             return new QuestBuilderPart(quest);
         }
 
-        public QuestBuilderPart Start(IQuest quest)
+        /// <summary>
+        /// Clone quest
+        /// </summary>
+        /// <param name="quest"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static QuestBuilderPart Start(IQuest quest)
         {
+#if DEBUG
+            if (quest is null) throw new ArgumentNullException(nameof(quest));
+#endif
+
             return new QuestBuilderPart((IQuest)quest.Clone());
         }
         
-        public class QuestBuilderPart
+        public struct QuestBuilderPart
         {
             private IQuest _quest;
             private IQuestAggregator _questAggregator;
@@ -48,19 +60,20 @@ namespace KarpikQuests
 
             public QuestBuilderPart(IQuest quest)
             {
+#if DEBUG
                 if (quest is null) throw new ArgumentNullException(nameof(quest));
+#endif
 
                 _quest = quest;
+                _questAggregator = null;
+                _addToAggregator = false;
             }
         
-            /// <summary>
-            /// Clone quest
-            /// </summary>
-            /// <param name="quest"></param>
-            /// <returns></returns>
-
-            public QuestBuilderPart AddBundle(ITaskBundle bundle)
+            public readonly QuestBuilderPart AddBundle(ITaskBundle bundle)
             {
+#if DEBUG
+                if (bundle is null) throw new ArgumentNullException(nameof(bundle));
+#endif
                 if (_quest.TaskBundles.Has(bundle)) throw new InvalidOperationException("Quest can't contain equel bundle");
 
                 _quest.AddBundle(bundle);
@@ -78,7 +91,7 @@ namespace KarpikQuests
                 return this;
             }
 
-            public QuestBuilderPart OnComplete(Action<IQuest> onComplete)
+            public readonly QuestBuilderPart OnComplete(Action<IQuest> onComplete)
             {
                 _quest.Completed += onComplete;
                 return this;
@@ -93,7 +106,7 @@ namespace KarpikQuests
                     _questAggregator.TryAddQuest(_quest);
                 }
 
-                IQuest quest = _quest;
+                var quest = _quest;
                 _quest = null;
                 return quest;
             }
